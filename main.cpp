be6591b3d594d0bd0c354eefb84c9c503cefc9d2
jojomblo <daunbergoyang344@gmail.com>
2025-07-03 19:59:47 +0700

    refactor: simplify main.cpp structure and implement shape tweening animation

--- main.cpp
+++ main.cpp
@@ -1,193 +1,110 @@
-// Jonathan Aditya Vandra 672022006
-#include <GL/freeglut.h>
-#include "texture/loadImageBmp.cpp"
-#include "texture/loadImageJpg.cpp"  
-#include "texture/loadImagePng.cpp" 
-#include "texture/loadImageWebp.cpp" 
-#include "texture/loadImageTga.cpp"
-#include "texture/loadImageDds.cpp"
-#include <string.h> 
+#include <GL/glut.h>
 
-float rotX = 20.0f;
-float rotY = 30.0f;
-bool autoRotate = false;
+#define RATIO 1.200
+#define WW 100
+#define WH (WW/RATIO)
+#define HALFX ((int)(WW/2))
+#define HALFY ((int)(WH/2))
+#define deltat .001
+int startTime = 0;
 
-GLuint textureSideID;
-GLuint textureFrontID;
-GLuint textureBackID;
-GLuint textureLeftID;
-GLuint textureTopID;
-GLuint textureBottomID;  
-
-void init() {
-    glEnable(GL_DEPTH_TEST);
-    glEnable(GL_TEXTURE_2D);
-    glClearColor(0.9f, 0.9f, 0.9f, 1.0f);
-
-    textureFrontID = loadBMP_custom("img/852.bmp");
-    textureBackID  = loadJPG_custom("img/662.jpg");
-    textureSideID = loadPNG_custom("img/346.png");
-    textureLeftID = loadWebP_custom("img/784.webp");
-    textureTopID = loadTGA_custom("img/691.tga");
-    textureBottomID = loadDDS_custom("img/289.dds");
-
-    glBindTexture(GL_TEXTURE_2D, textureBottomID);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+int WindowWidth;
+int WindowHeight;
 
 
-    glBindTexture(GL_TEXTURE_2D, textureTopID);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-
-    glBindTexture(GL_TEXTURE_2D, textureLeftID);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    glBindTexture(GL_TEXTURE_2D, textureSideID);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-
-    glBindTexture(GL_TEXTURE_2D, textureFrontID);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    glBindTexture(GL_TEXTURE_2D, textureBackID);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-}
-
-void display() {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glLoadIdentity();
-
-    gluLookAt(3.0, 3.0, 7.0,
-              0.0, 0.0, 0.0,
-              0.0, 1.0, 0.0);
-
-    glRotatef(rotX, 1.0f, 0.0f, 0.0f);
-    glRotatef(rotY, 0.0f, 1.0f, 0.0f);
-
-    // DEPAN (BMP)
-    glEnable(GL_TEXTURE_2D);
-    glBindTexture(GL_TEXTURE_2D, textureFrontID);
-    glBegin(GL_QUADS);
-    glColor3f(1, 1, 1);
-    glTexCoord2f(0, 0); glVertex3f(-1, -1, 1);
-    glTexCoord2f(1.25, 0); glVertex3f(1, -1, 1);
-    glTexCoord2f(1.25, 1); glVertex3f(1, 1, 1);
-    glTexCoord2f(0, 1); glVertex3f(-1, 1, 1);
-    glEnd();
-
-    // BELAKANG (JPG)
-    glBindTexture(GL_TEXTURE_2D, textureBackID);
-    glBegin(GL_QUADS);
-    glColor3f(1, 1, 1);
-    glTexCoord2f(1, 0); glVertex3f(-1, -1, -1);
-    glTexCoord2f(1, 1); glVertex3f(-1, 1, -1);
-    glTexCoord2f(0, 1); glVertex3f(1, 1, -1);
-    glTexCoord2f(0, 0); glVertex3f(1, -1, -1);
-    glEnd();
-
-    // KANAN (PNG)
-    glEnable(GL_TEXTURE_2D);
-    glBindTexture(GL_TEXTURE_2D, textureSideID);
-    glColor3f(1, 1, 1);
-    glBegin(GL_QUADS);
-    glTexCoord2f(0, 0); glVertex3f(1, -1, -1);
-    glTexCoord2f(0, 1); glVertex3f(1, 1, -1);
-    glTexCoord2f(1, 1); glVertex3f(1, 1, 1);
-    glTexCoord2f(1, 0); glVertex3f(1, -1, 1);
-    glEnd();
-
-    // KIRI (WEBP)
-    glEnable(GL_TEXTURE_2D);
-    glBindTexture(GL_TEXTURE_2D, textureLeftID);
-    glColor3f(1, 1, 1);
-    glBegin(GL_QUADS);
-    glTexCoord2f(0, 0); glVertex3f(-1, -1, -1);
-    glTexCoord2f(1, 0); glVertex3f(-1, -1, 1);
-    glTexCoord2f(1, 1); glVertex3f(-1, 1, 1);
-    glTexCoord2f(0, 1); glVertex3f(-1, 1, -1);
-    glEnd();
-
-    // ATAS (TGA)
-    glEnable(GL_TEXTURE_2D);
-    glBindTexture(GL_TEXTURE_2D, textureTopID);
-    glColor3f(1, 1, 1);
-    glBegin(GL_QUADS);
-    glTexCoord2f(0, 0); glVertex3f(-1, 1, -1);
-    glTexCoord2f(1, 0); glVertex3f(-1, 1, 1);
-    glTexCoord2f(1, 1); glVertex3f(1, 1, 1);
-    glTexCoord2f(0, 1); glVertex3f(1, 1, -1);
-    glEnd();
-
-    // BAWAH (DDS)
-    glEnable(GL_TEXTURE_2D);
-    glBindTexture(GL_TEXTURE_2D, textureBottomID);
-    glColor3f(1, 1, 1);
-    glBegin(GL_QUADS);
-    glTexCoord2f(0, 0); glVertex3f(-1, -1, -1);
-    glTexCoord2f(1, 0); glVertex3f(1, -1, -1);
-    glTexCoord2f(1, 1); glVertex3f(1, -1, 1);
-    glTexCoord2f(0, 1); glVertex3f(-1, -1, 1);
-    glEnd();
-    
-    glutSwapBuffers();
-}
-
-void reshape(int w, int h) {
-    glViewport(0, 0, w, h);
+void Init(){
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluPerspective(60.0, (float)w / h, 1.0, 100.0);
+    gluOrtho2D(-HALFX, HALFX, -HALFY, HALFY);
     glMatrixMode(GL_MODELVIEW);
+    glClearColor(0, 0, 0, 0);
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glEnableClientState(GL_COLOR_ARRAY);
+    glShadeModel(GL_SMOOTH);
+    glViewport(0, 0, WindowWidth, WindowHeight);
 }
 
-void timer(int) {
-    if (autoRotate) {
-        rotX += 0.3f;
-        rotY += 0.5f;
-        if (rotX > 360) rotX -= 360;
-        if (rotY > 360) rotY -= 360;
-        glutPostRedisplay();
+
+void Display(){
+    glLineWidth(4.0);
+
+    float startShape[12][2] = {
+        {10, 25}, {10, -15},  // Sisi kanan luar
+        {5, -20}, {-10, -20}, // Lengkungan bawah luar
+        {-15, -15}, {-15, -5},// Sisi kiri luar
+        {-5, -5}, {-5, -10},  // Sisi kiri dalam
+        {0, -10}, {0, -10},   // Lengkungan bawah dalam
+        {0, 20}, {-5, 25}     // Bagian atas dalam
+    };
+
+     float endShape[12][2] = {
+        {-5, 20}, {10, 20},  // Sisi atas luar
+        {15, 15}, {15, 5},    // Lengkungan kanan atas
+        {10, 0}, {15, -5},    // Tonjolan tengah
+        {15, -15}, {10, -20}, // Lengkungan kanan bawah
+        {-5, -20}, {5, -10},// Sisi bawah & kiri dalam
+        {0, 0}, {5, 10}      // Bagian tengah & atas dalam
+    };
+
+    float intermediateShape[12][2];
+    float vertexColor[12][3] = {
+        {1, 0, 0},{1, 1, 0},{1, 0, 1},{0, 1, 0},
+        {0, 1, 1},{0, 0, 1},{1, 0.5, 0},{1, 0, 0.5},
+        {0.5, 1, 0},{0.5, 0, 1},{1, 0, 0.5},{0, 1, 0.5}
+    };
+
+    // Hitung waktu sejak start
+    int currentTime = glutGet(GLUT_ELAPSED_TIME);
+    float elapsedSeconds = (currentTime - startTime) / 1000.0f;
+
+    static float Tween = 0.0f;
+
+    // Freeze selama 2 detik
+    if (elapsedSeconds >= 2.0f && Tween < 1.0f) {
+        Tween += deltat;
     }
-    glutTimerFunc(16, timer, 0);
-}
 
-void specialKeys(int key, int, int) {
-    if (!autoRotate) {
-        switch (key) {
-        case GLUT_KEY_UP:    rotX -= 5.0f; break;
-        case GLUT_KEY_DOWN:  rotX += 5.0f; break;
-        case GLUT_KEY_LEFT:  rotY -= 5.0f; break;
-        case GLUT_KEY_RIGHT: rotY += 5.0f; break;
-        }
-        glutPostRedisplay();
+    // Lerp antara start dan end
+    for (int Vtx = 0; Vtx < 12; Vtx++) {
+        intermediateShape[Vtx][0] = (1.0f - Tween) * startShape[Vtx][0] + Tween * endShape[Vtx][0];
+        intermediateShape[Vtx][1] = (1.0f - Tween) * startShape[Vtx][1] + Tween * endShape[Vtx][1];
     }
+
+    glVertexPointer(2, GL_FLOAT, 0, intermediateShape);
+    glColorPointer(3, GL_FLOAT, 0, vertexColor);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glDrawArrays(GL_LINE_LOOP, 0, 12);
+
+    glutSwapBuffers();
+    glutPostRedisplay();
 }
 
-void keyboard(unsigned char key, int, int) {
-    if (key == 32) {
-        autoRotate = !autoRotate;
-        glutPostRedisplay();
-    }
+
+
+void Reshape(int w, int h){
+    WindowWidth = w;
+    WindowHeight = (int)(w / RATIO);
+    glViewport(0, 0, w, WindowHeight);
+    Init();
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv){
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-    glutInitWindowSize(600, 600);
-    glutCreateWindow("Kubus BerTexture");
-    init();
-    glutDisplayFunc(display);
-    glutReshapeFunc(reshape);
-    glutTimerFunc(0, timer, 0);
-    glutKeyboardFunc(keyboard);
-    glutSpecialFunc(specialKeys);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+    WindowWidth = (int)(glutGet((GLenum)GLUT_SCREEN_WIDTH) * 0.4);
+    WindowHeight = (int)(WindowWidth / RATIO);
+    glutInitWindowSize(WindowWidth, WindowHeight);
+    glutInitWindowPosition((int)(glutGet((GLenum)GLUT_SCREEN_WIDTH) * 0.1), (glutGet((GLenum)GLUT_SCREEN_HEIGHT) / 2) - (WindowHeight / 2));
+    
+    glutCreateWindow("Tweening J ke 3");
 
+    startTime = glutGet(GLUT_ELAPSED_TIME); // ⏱️ Simpan waktu awal
+
+    glutDisplayFunc(Display);
+    glutReshapeFunc(Reshape);
+    Init();
     glutMainLoop();
+    
     return 0;
 }
